4bb44f1f05b1ff35aa0ecbea453aedeffcb09028
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-06 00:43:09 +0300

    Update full-backup.sh

--- full-backup.sh
+++ full-backup.sh
@@ -4,18 +4,40 @@
 # Variables
 #########################
 
-source configure_backup
+source configure_backup 2> /dev/null
 
 #########################
 # Full Backup
 #########################
 
-mkdir -p $BACKUP_DIR
-mkdir -p $FULL_DST/$TIMESTAMP
+if [ -d "$FULL_DST/$TIMESTAMP" ]
+then
+        echo "Сегодня уже был создан полный бэкап"
+else
+    
+    mkdir -p $BACKUP_DIR
+    mkdir -p $FULL_DST/$TIMESTAMP
 
-xtrabackup --backup --databases $DATABASE --target-dir=$FULL_DST/$TIMESTAMP
-xtrabackup --prepare --target-dir=$FULL_DST/$TIMESTAMP
+    echo "Создаем полную резервную копию"
+    
+    xtrabackup --backup --databases $DATABASE --target-dir=$FULL_DST/$TIMESTAMP &> /tmp/full_backup_$TIMESTAMP.log
 
-chown -R mysql:mysql $FULL_DST/$TIMESTAMP
+    echo "Подготавливаем резервную копию для восстановления"
+    
+    xtrabackup --prepare --target-dir=$FULL_DST/$TIMESTAMP &> /tmp/full_prepare_backup_$TIMESTAMP.log
 
-find $FULL_DST -mtime +14 -exec rm -rf {} \;
+    echo "Назначаем корректного владельца и группу для MySQL"
+
+    chown -R mysql:mysql $FULL_DST/$TIMESTAMP
+
+    echo "Удаляем все резервные копии старше 14 дней"
+
+    find $FULL_DST -mtime +14 -exec rm -rf {} \;
+
+    echo "Задача полного резервного копирования завершена, полную резервную копию можно найти по указанному пути ниже"
+
+    ls -ld $FULL_DST/$TIMESTAMP | cut -c 43-
+
+    echo " "
+
+fi
