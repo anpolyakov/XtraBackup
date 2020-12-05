a37b2af4241bfb1af3374b295aa85c197b80667b
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-06 00:43:31 +0300

    Update full-restore.sh

--- full-restore.sh
+++ full-restore.sh
@@ -10,6 +10,22 @@ source configure_backup
 # Full Restore
 #########################
 
+echo "Останавливаем MySQL"
+
 systemctl stop mysql
+
+echo "Удаляем исходную базу данных"
+
 rm -rf /var/lib/mysql/$DATABASE
+
+echo "Восстанавливаем полный бэкап"
+
 cp -Rp $FULL_DST/$LAST_FULL_BACKUP/$DATABASE /var/lib/mysql/$DATABASE
+
+echo "Задаем корректного владельца и группу для восстановленной базы"
+
+chown -R mysql:mysql /var/lib/mysql/$DATABASE
+
+echo "Запускаем MySQL"
+
+systemctl start mysql
