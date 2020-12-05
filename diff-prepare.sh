2f4dafbd8b3f9ed3eec0ec42374beaa62f62094b
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-06 00:42:27 +0300

    Update diff-prepare.sh

--- diff-prepare.sh
+++ diff-prepare.sh
@@ -10,10 +10,21 @@ source configure_backup
 # Diff Restore
 #########################
 
-xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP
-xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$LAST_DIFF_BACKUP
+echo "Подготавливаем полный бэкап к восстановлению"
+
+xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP &> /tmp/diff_prepare_backup_$TIMESTAMP.log
+
+echo "Подготавливаем первичный инкрементный бэкап к восстановлению"
+
+xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$LAST_DIFF_BACKUP \
+
+&>> /tmp/diff_prepare_backup_$TIMESTAMP.log
 
 for value in ${array_next[@]}
 do
-        xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$value
+        xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$value \
+        &>> /tmp/diff_prepare_backup_$TIMESTAMP.log
+        echo "Подготавливаем следующий доступный инкрементный бэкап для восстановления"
 done
+
+echo "Применение всех доступных инкрементных бэкапов от $TIMESTAMP к полному бэкапу завершено"
