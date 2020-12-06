4de4efbccd5aa1326dd7a1efdcc4f22ca0a026be
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-06 23:28:46 +0300

    Update diff-prepare.sh

--- diff-prepare.sh
+++ diff-prepare.sh
@@ -16,9 +16,7 @@ xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP &>
 
 echo "Подготавливаем первичный инкрементный бэкап к восстановлению"
 
-xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$LAST_DIFF_BACKUP \
-
-&>> /tmp/diff_prepare_backup_$TIMESTAMP.log
+xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$LAST_DIFF_BACKUP &>> /tmp/diff_prepare_backup_$TIMESTAMP.log
 
 for value in ${array_next[@]}
 do
