4052e9acb65c1c089a182fa6c441c5eb42beb6f5
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-07 18:23:58 +0300

    Update cron-setup.sh

--- cron/cron-setup.sh
+++ cron/cron-setup.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+# Файл для создания cron задач по расписанию, при помощи скрипта
+
 #########################
 # Variables
 #########################
 
-source configure_cron 2> /dev/null
+source configure_cron 2> /dev/null # импорт внешнего файла configure_cron, при этом импортируются все переменные и другие параметры
 
-echo LOCATION=$LOCATION >> $HOME/.bashrc
-source $HOME/.bashrc
+echo LOCATION=$LOCATION >> $HOME/.bashrc # считывается переменная $LOCATION из файла configure_backup и экспортируется в файл .bashrc
+                                         # для того, чтобы она постоянно присутствовала в системе, это необходимо для выполнения cron-задач
+source $HOME/.bashrc                     # импорт внешнего файла .bashrc
 
 #########################
 # Cron Schedule Setup
