26a3c12223f34cdc1aaf450ba7c7df5dd8345c24
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-07 18:09:43 +0300

    Update diff-backup_next.sh

--- diff-backup_next.sh
+++ diff-backup_next.sh
@@ -4,7 +4,7 @@
 # Variables
 #########################
 
-source configure_backup 2> /dev/null
+source configure_backup 2> /dev/null # подключение внешнего файла configure_backup, при этом импортируются перменные и другие параметры
 
 #########################
 # Diff Next Backup
@@ -14,15 +14,17 @@ echo "Первичная инкрментная резервная копия у
 
 echo "Создаем дополнительную инкрементную резервную копию"
 
+# создание дополнительного инкрементного бэкапа, он создается после первичного инкрементного бэкапа
+
 xtrabackup --databases=$DATABASE --backup --target-dir=$DIFF_DST/$LAST_DIFF_BACKUP-$TIME --incremental-basedir=$DIFF_DST/$DIFF_BASE_DIR &> /tmp/diff_next_backup_$TIMESTAMP.log 
 
 echo "Назначаем корректного владельца и группу MySQL"
 
-chown -R mysql:mysql $DIFF_DST/$TIMESTAMP
+chown -R mysql:mysql $DIFF_DST/$TIMESTAMP # назначаем корректного владельца и группу
 
 echo "Удаляем резервные копии старше 14 дней"
 
-find $DIFF_DST -mtime +14 -exec rm -rf {} \;
+find $DIFF_DST -mtime +14 -exec rm -rf {} \; # удаляются инкрементные резервные копии страше 14 дней
 
 echo "Создание дополнительной инкрементной резервной копии завершено"
 
