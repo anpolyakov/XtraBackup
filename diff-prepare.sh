2153c468a188956184faba57284c399042755979
Aleksandr Polyakov <43615585+anpolyakov@users.noreply.github.com>
2020-12-07 18:13:29 +0300

    Update diff-prepare.sh

--- diff-prepare.sh
+++ diff-prepare.sh
@@ -4,7 +4,7 @@
 # Variables
 #########################
 
-source configure_backup
+source configure_backup. # подключение внешнего файла configure_backup, при этом импортируются все переменные из файла и другие параметры
 
 #########################
 # Diff Restore
@@ -12,11 +12,17 @@ source configure_backup
 
 echo "Подготавливаем полный бэкап к восстановлению"
 
+# подготовка полного бэкапа для восстановления
+
 xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP &> /tmp/diff_prepare_backup_$TIMESTAMP.log
+
+# подготовка первичного инкрементного бэкапа для восстановления
 
 echo "Подготавливаем первичный инкрементный бэкап к восстановлению"
 
 xtrabackup --prepare --apply-log-only --target-dir=$FULL_DST$LAST_FULL_BACKUP --incremental-dir=$DIFF_DST/$LAST_DIFF_BACKUP &>> /tmp/diff_prepare_backup_$TIMESTAMP.log
+
+# Подготавливает все инкрементные бэкапы на текущую дату для восстановления
 
 for value in ${array_next[@]}
 do
